294698cae05b8cf69709fb53f606a274dba451e7
dodotronix <dodotronix@gmail.com>
2024-09-11 00:09:54 +0200

    finished simulation of the speed when the movable part reaches the top

--- sim/kinematic_model.m
+++ sim/kinematic_model.m
@@ -13,7 +13,8 @@ press_coef = 0.5; % F/t
 % hights of the object and the hight of the seling
 h = 200; % m
 hr = 1; % m
-h_soft = 20; % m - it's a distance where the motor starts to decelerate
+h_soft = 50; % m - it's a distance where the motor starts to decelerate
+h_soft_ratio = 0.1; % percentage where the speed drops to the v_decel
 
 % example inputs
 % v = 12e-3 % m/s
@@ -21,10 +22,13 @@ h_soft = 20; % m - it's a distance where the motor starts to decelerate
 
 v0 = 0;
 v1 = 10; % m/s
+v_decel = 5; % m/s
 accel = 2; % m/s^2
+t_pressed = 10; % s - number of seconds when the two planes are pressing each other
 
 % deceleration is calculated based on the soft switch limit
-decel = -1*v1^2/(2*h_soft)
+t_decel = (2*h_soft_ratio*h_soft)/(v1+v_decel) 
+decel = -1*(v1-v_decel)/t_decel
 
 % adc sampling
 fs = 1000; % Hz
@@ -43,16 +47,21 @@ t2_vec = [1/fs:1/fs:t2];
 v2_vec = v1*ones(1,length(t2_vec));
 
 % decelaration stage - estimation of the distance
-t3_vec = [0:1/fs:(v0-v1/decel)-1/fs];
-v3_vec = v1+decel*t3_vec; 
+t3_vec_a = [0:1/fs:t_decel-1/fs];
+v3_vec_a = v1+decel*t3_vec_a; 
+
+t3_vec_b = [0:1/fs:(1-h_soft_ratio)*h_soft/v_decel-1/fs];
+v3_vec_b = v_decel*ones(1,length(t3_vec_b));
+
+t3_vec = [t3_vec_a t3_vec_b];
+v3_vec = [v3_vec_a v3_vec_b];
 
 % % touch stage the speed is zero
-% vstop = 0;
-% s4 0;
-% t4 = 2*(t1+t2+t3);
+t4_vec = [0:1/fs:t_pressed-1/fs];
+v4_vec = zeros(1,length(t4_vec));
 
 % concat all vectors
-v_vec = [v1_vec v2_vec v3_vec];
+v_vec = [v1_vec v2_vec v3_vec v4_vec];
 t_vec = [0:length(v_vec)-1]/fs;
 
 length(t_vec)
