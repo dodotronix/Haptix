f0eb3b3e4905cf19aa879105107c76c5d1d49699
dodotronix <dodotronix@gmail.com>
2024-09-17 16:23:52 +0200

    blink test python script using ft232h

---
+++ fw/accel.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+os.environ['BLINKA_FT232H'] = "1"
+
+import board
+import digitalio
+
+from pyftdi.ftdi import Ftdi
+from time import sleep
+
+
+# Blink test
+# led = digitalio.DigitalInOut(board.C0)
+# led.direction = digitalio.Direction.OUTPUT
+
+
+# while True:
+#     led.value = True
+#     sleep(0.5)
+#     led.value = False
+#     sleep(0.5)
