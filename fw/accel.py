73dbc7691c083c953ba83cd47d01699e2ff6ce00
dodotronix <dodotronix@gmail.com>
2024-09-21 13:13:46 +0200

    added spi, it seems to be responding, but the ft232h bridge does not support cpha=1

--- fw/accel.py
+++ fw/accel.py
@@ -10,6 +10,7 @@ import busio
 
 from time import sleep
 # from adafruit_bno08x.i2c import BNO08X_I2C
+from adafruit_bno08x.spi import BNO08X_SPI
 from adafruit_bno08x import BNO_REPORT_ACCELEROMETER
 
 
@@ -19,7 +20,12 @@ log.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
 if __name__ == '__main__':
 
     # i2c = busio.I2C(board.SCL, board.SDA, frequency=100_000)
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    spi_cs = digitalio.DigitalInOut(board.C0)
+    _intpin = digitalio.DigitalInOut(board.C7)
+    _resetpin = digitalio.DigitalInOut(board.D7)
     # bno = BNO08X_I2C(i2c, address=0x4b, debug=True)
+    bno = BNO08X_SPI(spi, cspin=spi_cs, intpin=_intpin, resetpin=_resetpin, debug=True)
     bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 
 # Blink test
