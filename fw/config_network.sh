494f3bcaad811f4c6f906433c24eb09358003ef4
dodotronix <dodotronix@gmail.com>
2024-10-17 10:51:11 +0200

    added network configuration to connect rigol to my computer

---
+++ fw/config_network.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# config network on belzebub
+ip link set up dev enp0s31f6
+ip addr add 10.0.0.2/24 dev enp0s31f6
+
+# NOTE rigol network configuration in /etc/dhcpcd.conf
+# option subnet-mask 255.255.255.0;
+# subnet 10.0.0.0 netmask 255.255.255.0 {
+#   range 10.0.0.0 10.0.0.20;
+# }
+
+# host rigol_scope{
+#   hardware ethernet 00:19:AF:34:26:A3;
+#   fixed-address 10.0.0.14;
+# }
+
+# start dhcpd
+systemctl start dhcpcd4@enp0s31f6
