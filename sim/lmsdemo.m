5465db4bf53cb49f02abedc5539151bd3828c9e9
dodotronix <dodotronix@protonmail.com>
2025-03-29 11:37:09 +0100

    lms demo

--- sim/lmsdemo.m
+++ sim/lmsdemo.m
@@ -12,21 +12,26 @@ f1 = 1e3;
 f2 = 5.1e2;
 f3 = 2.4e3;
 
-a1 = 1;
-a2 = 1;
-a3 = 1;
+a1 = 0.33;
+a2 = 0.8;
+a3 = 0.54;
 
-t = [0:50*fs/f1]*Ts;
+t = [0:100*fs/f1-1]*Ts;
 
-
-s1 = a1*sin(2*pi*f1*t);
-
-s2 = a2*sin(2*pi*f2*t);
-
-s3 = a3*sin(2*pi*f3*t);
+s1 = sin(2*pi*f1*t);
+s2 = sin(2*pi*f2*t);
+s3 = sin(2*pi*f3*t);
 
 DC = 6;
-mixed_s = 0.1*s1 + 0.33*s2 + 2*s3 + DC;
+
+pwidth = 4e2;
+pdelay = 2e3;
+pulse = 2*[zeros(1, pdelay), ones(1, pwidth), zeros(1, length(t) - pwidth - pdelay)];
+mixed_s = a1*s1 + a2*s2 + a3*s3 + pulse + DC;
+
+%s1 = [s1(101:end) s1(1:100)];
+%s2 = [s2(101:end) s2(1:100)];
+%s3 = [s3(101:end) s3(1:100)];
 
 figure;
 subplot(2, 1, 1)
@@ -43,29 +48,32 @@ grid on
 
 % LMS algorithm
 
-alpha = 0.9e-3;
-N = 2;
-W = zeros(3, 1);
-e = zeros(N, 1);
+alpha = 1e-2;
+W = zeros(4, 1);
+e = 0;
 err = zeros(1, length(t));
+w = zeros(length(t), 4);
 
 
-for n = 1:length(t)-N
-  x = [s1(n:n+N-1)', ...
-       s2(n:n+N-1)', ...
-       s3(n:n+N-1)'];
+for n = 1:length(t)
+  x = [ 1; ...
+       s1(n); ...
+       s2(n); ...
+       s3(n)];
 
-  d = mixed_s(n:n+N-1)';
+  d = mixed_s(n);
 
-  e = d - x*W;
-
-  W = W + 2*alpha*x'*e;
-  err(n:n+N-1) = e;
-
+  e = d - W'*x;
+  W = W + 2*alpha*x*e;
+  err(n) = e;
+  w(n, :) = W;
 end
-
 
 figure;
 plot(err)
+grid on
+
+figure;
+plot(w)
 grid on
 
