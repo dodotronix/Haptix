b6914662570189f356fc8b2d57adb23a4aba3979
dodotronix <dodotronix@gmail.com>
2025-05-11 11:53:22 +0200

    simple butterworth filtering, scaling and subtraction of the signals

--- fw/arduino/haptix_full_procchain.ino
+++ fw/arduino/haptix_full_procchain.ino
@@ -1,158 +1,150 @@
-#include <stdio.h>
-#include <Adafruit_MCP3008.h>
-
 #define YELLOW 6
 #define BLUE 7
-#define BUTTER_SIZE 2
+#define TIMER_PERIOD 59135 
+
+#define BUTTER_SIZE 3
 #define BUFFER_SIZE 8
 #define BUFFER_MASK (BUFFER_SIZE) - 1
-#define OFFSET_MEAS_LENGTH 10
+#define OFFSET_MEAS_LENGTH 4 // i can go up to 16
 
-#define ALPHA 0.01f
-#define BETA 0.08f
+#define Q 15 
+#define ONE_Q 32767
+#define ADC_WIDTH 10
+#define ADC_TO_Q115( x ) ( (x - 512) << (Q - ADC_WIDTH) )
 
+#define FACTOR 18350 // 0.56
+// #define FACTOR 32767 // ~1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Adafruit_MCP3008.h>
+
+// constants
+const int a[3] = {32767, 3277, 328};
+const int b[3] = {16384, 1638, 164};
 
 typedef struct {
-    float in[BUFFER_SIZE];
-    float out[BUFFER_SIZE];
-    float offset;
-    float ema;
-    float ema2;
-    float var;
-    float gradient;
-    float factor;
-    int channel;
-    int delay;
-    int head;
+    int in[BUFFER_SIZE];
+    int out[BUFFER_SIZE];
+    int offset;
+    int factor;
+    uint8_t dly;
+    uint8_t head;
 } BF_t; // input filter circular buffer
 
-const float a[2] = {1.0, -0.8677558};
-const float b[2] = {0.0661221, 0.0661221};
-
-Adafruit_MCP3008 adc;
-BF_t force;
-int touched;
-
-volatile uint8_t trigger = 0;
-
-void butter_init(BF_t *filter, int channel, int delay, float factor, unsigned char *trigger){
-    filter->channel = channel;
-    filter->offset = 0;
-    filter->head = 0;
-    filter->ema = 0;
-    filter->ema2 = 0;
-    filter->var = 0;
-    filter->gradient = 0;
-    filter->factor = factor;
-    filter->delay = delay;
-    memset(filter->in, 0, sizeof(filter->in));
-    memset(filter->out, 0, sizeof(filter->out));
-
-    // calculate offset 
-#if OFFSET_MEAS_LENGTH
-    for(int i=0; i<OFFSET_MEAS_LENGTH; ++i)
-        while(!*trigger); // wait for data ready
-        *trigger = 0;
-        filter->offset += (float)adc.readADC(filter->channel);
-    filter->offset /= OFFSET_MEAS_LENGTH;
-#endif
-
-    printf("offset: %f\n", filter->offset);
+// NOTE pramater a can be bigger 
+// than one up to certain extend
+// this is intended to be used 
+// in the detector function
+int fmul(uint32_t a, int b){
+  // TODO add missing overflow exception
+  uint32_t tmp = (a*b + (1UL << (Q-1)));
+  return (int)(tmp >> Q);
 }
 
-float butter_get_nth(BF_t *filter, unsigned char n) {
+void butter_init(BF_t *filter, int offset, int factor, uint8_t dly){
+    filter->head = 0;
+    filter->dly = dly;
+    filter->factor = factor;
+    filter->offset = offset;
+    memset(filter->in, 0, sizeof(filter->in));
+    memset(filter->out, 0, sizeof(filter->out));
+}
+
+int butter_get_nth(BF_t *filter, unsigned char n) {
   unsigned char index = (filter->head - n) & BUFFER_MASK;
   return filter->out[index];
 }
 
 // butterworth filter function 
-void butter_push(BF_t *filter) {
+int butter_push(BF_t *filter, int value) {
     // shift in the new value
     filter->head = (filter->head + 1) & BUFFER_MASK;
-    float obtained = (float)adc.readADC(filter->channel) - filter->offset;
-    filter->in[filter->head] = filter->factor * obtained;
-    printf("filter input: %f\n", filter->in[filter->head]);
+    int obtained = value - filter->offset;
+    filter->in[filter->head] = fmul(filter->factor, obtained);
 
-    float result = 0; 
-    //printf("%f\n", filter->in[0]);
-
+    uint32_t result = 0; 
     for (int i = 0; i < BUTTER_SIZE; ++i) {
-        int idx = (filter->head - filter->delay - i) & BUFFER_MASK;
-        //printf("a%i %i %f\n", i, idx, filter->in[idx]);
-        result += b[i] * filter->in[idx];
+        int idx = (filter->head - filter->dly - i) & BUFFER_MASK;
+        result += fmul(b[i], filter->in[idx]);
     }
 
     for (int i = 1; i < BUTTER_SIZE; ++i) {
         int idx = (filter->head - i) & BUFFER_MASK;
-        //printf("b%i %i %f\n", i, idx, filter->out[idx]);
-        result -= a[i] * filter->out[idx];
+        result -= fmul(a[i], filter->out[idx]);
     }
 
-    // update ema, ema2 and var
-    filter->ema = ALPHA*(result) + (1 - ALPHA)*(filter->ema);
-    //printf("EMA: %f\n", filter->ema);
-    filter->ema2 = BETA*(result*result) + (1 - BETA)*(filter->ema2);
-    //printf("EMA2: %f\n", filter->ema2);
-    filter->var = filter->ema2 - (filter->ema)*(filter->ema);
-    //printf("VAR: %f\n", filter->var);
-    filter->gradient = result - butter_get_nth(filter, 2);
-    //printf("gradient: %f\n", filter->gradient);
-
     filter->out[filter->head] = result;
-    printf("filter out: %f\n\n", result);
+    return result;
 }
 
-uint8_t detector(BF_t *sens0, volatile uint8_t *trigger) {
-  if (!(*trigger))
-    return 0;
-
-  *trigger = 0;
-  butter_push(sens0);
-
-  //simple detect threshold
-  if(butter_get_nth(sens0, 0) > 20)
-    return 1;
-  return 0;
-}
+Adafruit_MCP3008 adc;
+volatile uint8_t trigger = 0;
+int measured[2] = {0, 0};
+int signal = 0;
+BF_t force, accel;
 
 void setup() {
+
+  uint32_t offset[2] = {0, 0};
+
   cli();
   TCCR1A = 0;           // Init Timer1A
   TCCR1B = 0;           // Init Timer1B
   TCCR1B |= B00000001;  // Prescaler = 1
-  TCNT1 = 63135;        // Timer Preloading
+  TCNT1 = TIMER_PERIOD;        // Timer Preloading
   TIMSK1 |= B00000001;  // Enable Timer Overflow Interrupt
   sei();
-
-  // set pin 
-  pinMode(YELLOW, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  digitalWrite(YELLOW, HIGH);
 
   Serial.begin(115200);
   while (!Serial);
 
-  Serial.println("Running Haptix proof-of-concept program\n");
+  Serial.println("Haptix initialization sequence\n");
+
+  // set pins 
+  pinMode(YELLOW, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+
+  // initialize ADC
   adc.begin(10);
 
-  //initialize
-  butter_init(&force, 0, 0, 1, &trigger);
+// find offset
+#if OFFSET_MEAS_LENGTH
+    for(int i=0; i<OFFSET_MEAS_LENGTH; ++i) {
+      while(!trigger); //wait for trigger
+      measured[0] = adc.readADC(0);
+      measured[1] = adc.readADC(1);
+      offset[0] += ADC_TO_Q115(measured[0]);
+      offset[1] += ADC_TO_Q115(measured[1]);
+    }
 
-  digitalWrite(BLUE, HIGH);
-  digitalWrite(YELLOW, LOW);
+    // rounding and division
+    offset[0] = ((offset[0] + (1 << 1)) >> 2);
+    offset[1] = ((offset[1] + (1 << 1)) >> 2);
+#endif
+
+  // initialize input buffer
+  butter_init(&force, (int)offset[0], ONE_Q, 3);
+  butter_init(&accel, (int)offset[1], FACTOR, 0);
 }
 
 ISR(TIMER1_OVF_vect){
-  TCNT1 = 63135; // Timer Preloading
+  TCNT1 = TIMER_PERIOD; // Timer Preloading
   trigger = 1;
 }
 
 void loop() {
-    if(touched){
-      digitalWrite(YELLOW, HIGH);
-      digitalWrite(BLUE, LOW);
-    } else {
-      digitalWrite(YELLOW, LOW);
-      digitalWrite(BLUE, HIGH);
-    }
+  if(trigger){
+    trigger = 0;
+    digitalWrite(YELLOW, HIGH);
+    measured[0] = adc.readADC(0);
+    measured[1] = adc.readADC(1);
+    signal = butter_push(&force, ADC_TO_Q115(measured[0]));
+    signal -= butter_push(&accel, ADC_TO_Q115(measured[1]));
+
+    if(signal > 328) digitalWrite(BLUE, HIGH);
+    else digitalWrite(BLUE, LOW);
+
+    digitalWrite(YELLOW, LOW);
+  }
 }
