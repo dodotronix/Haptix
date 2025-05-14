d6208048f55c3e5d5ae4c1083436fa5d8d3f5013
dodotronix <dodotronix@gmail.com>
2025-05-14 14:28:14 +0200

    updated detector processing to reduce number of bitshifts - cross-checked with the python script

--- fw/arduino/haptix_detector_test.ino
+++ fw/arduino/haptix_detector_test.ino
@@ -8,14 +8,20 @@
 #define TO_FLOAT( x ) ( ( (float)x ) / ( 1UL << Q ) )
 #define ADC_TO_Q115( x ) ( (x - 512) << (Q - ADC_WIDTH) )
 
-#define ZERO_SCORE_LAGS 4
+#define ZERO_SCORE_LAGS 64
+#define ZERO_SCORE_DIV 6
+
+#define ZERO_SCORE_EXTEND 2
 #define ZERO_SCORE_ALPHA 328 // 0.01
 #define ZERO_SCORE_NOISE  983 // 0.03
 #define ZERO_SCORE_THRES (4UL << Q)
+#define SQRT_PRECISION 8
 
 // static debugging
 #define DATA_LENGTH 11
 
+// the avg is window sum, you have to do the 
+// bit shift to get the real average value
 typedef struct {
     int buffer[ZERO_SCORE_LAGS];
     int avg;
@@ -24,50 +30,89 @@ typedef struct {
     unsigned char head;
 } ZSD_t; // zero-score detector
 
-// input format q1.15 -> sqrt() -> q1.15
+// input format q1.(15 + 2*EXTEND + DIV - PRECISION) -> sqrt() -> q1.15
 int sqrt_lut [256] = {
-  0, 181, 256, 314, 362, 405, 443, 479, 512, 543, 572,
-  600, 627, 653, 677, 701, 724, 746, 768, 789, 810, 830,
-  849, 868, 887, 905, 923, 941, 958, 975, 991, 1008, 1024,
-  1040, 1056, 1071, 1086, 1101, 1116, 1130, 1145, 1159, 1173, 1187,
-  1201, 1214, 1228, 1241, 1254, 1267, 1280, 1293, 1305, 1318, 1330,
-  1342, 1355, 1367, 1379, 1390, 1402, 1414, 1425, 1437, 1448, 1459,
-  1471, 1482, 1493, 1504, 1515, 1525, 1536, 1547, 1557, 1568, 1578,
-  1588, 1599, 1609, 1619, 1629, 1639, 1649, 1659, 1669, 1679, 1688,
-  1698, 1708, 1717, 1727, 1736, 1746, 1755, 1764, 1774, 1783, 1792,
-  1801, 1810, 1819, 1828, 1837, 1846, 1855, 1864, 1872, 1881, 1890,
-  1899, 1907, 1916, 1924, 1933, 1941, 1950, 1958, 1966, 1975, 1983,
-  1991, 1999, 2008, 2016, 2024, 2032, 2040, 2048, 2056, 2064, 2072,
-  2080, 2088, 2095, 2103, 2111, 2119, 2126, 2134, 2142, 2149, 2157,
-  2165, 2172, 2180, 2187, 2195, 2202, 2210, 2217, 2224, 2232, 2239,
-  2246, 2254, 2261, 2268, 2275, 2283, 2290, 2297, 2304, 2311, 2318,
-  2325, 2332, 2339, 2346, 2353, 2360, 2367, 2374, 2381, 2388, 2395,
-  2401, 2408, 2415, 2422, 2429, 2435, 2442, 2449, 2455, 2462, 2469,
-  2475, 2482, 2489, 2495, 2502, 2508, 2515, 2521, 2528, 2534, 2541,
-  2547, 2554, 2560, 2566, 2573, 2579, 2585, 2592, 2598, 2604, 2611,
-  2617, 2623, 2629, 2636, 2642, 2648, 2654, 2660, 2667, 2673, 2679,
-  2685, 2691, 2697, 2703, 2709, 2715, 2721, 2727, 2733, 2739, 2745,
-  2751, 2757, 2763, 2769, 2775, 2781, 2787, 2793, 2798, 2804, 2810,
-  2816, 2822, 2828, 2833, 2839, 2845, 2851, 2856, 2862, 2868, 2874,
-  2879, 2885, 2891 
+  0, 91, 128, 157, 181, 202, 222, 239, 256, 272, 286, 300,
+  314, 326, 339, 351, 362, 373, 384, 395, 405, 415, 425, 434,
+  443, 453, 462, 470, 479, 487, 496, 504, 512, 520, 528, 535,
+  543, 551, 558, 565, 572, 580, 587, 594, 600, 607, 614, 621,
+  627, 634, 640, 646, 653, 659, 665, 671, 677, 683, 689, 695,
+  701, 707, 713, 718, 724, 730, 735, 741, 746, 752, 757, 763,
+  768, 773, 779, 784, 789, 794, 799, 804, 810, 815, 820, 825,
+  830, 834, 839, 844, 849, 854, 859, 863, 868, 873, 878, 882,
+  887, 891, 896, 901, 905, 910, 914, 919, 923, 927, 932, 936,
+   941, 945,  949,   954,  958,  962,  966,  971,  975,  979,  983,  987,
+   991,  996, 1000, 1004, 1008, 1012, 1016, 1020, 1024, 1028, 1032, 1036,
+  1040, 1044, 1048, 1052, 1056, 1059, 1063, 1067, 1071, 1075, 1079, 1082,
+  1086, 1090, 1094, 1097, 1101, 1105, 1109, 1112, 1116, 1120, 1123, 1127,
+  1130, 1134, 1138, 1141, 1145, 1148, 1152, 1156, 1159, 1163, 1166, 1170,
+  1173, 1177, 1180, 1184, 1187, 1190, 1194, 1197, 1201, 1204, 1208, 1211,
+  1214, 1218, 1221, 1224, 1228, 1231, 1234, 1238, 1241, 1244, 1248, 1251,
+  1254, 1257, 1261, 1264, 1267, 1270, 1274, 1277, 1280, 1283, 1286, 1290,
+  1293, 1296, 1299, 1302, 1305, 1308, 1312, 1315, 1318, 1321, 1324, 1327,
+  1330, 1333, 1336, 1339, 1342, 1346, 1349, 1352, 1355, 1358, 1361, 1364,
+  1367, 1370, 1373, 1376, 1379, 1382, 1385, 1387, 1390, 1393, 1396, 1399,
+  1402, 1405, 1408, 1411, 1414, 1417, 1420, 1422, 1425, 1428, 1431, 1434,
+  1437, 1440, 1442, 1445
 };
 
 int fsqrt(int x){
-    if (x > 255) {
-        return sqrt_lut[255]; 
-    } else {
-        return sqrt_lut[x];
-    }
+  int index = (x >> SQRT_PRECISION);
+
+  if (index > 255) {
+    Serial.print("(LUT SQRT Overflow) ");
+    return sqrt_lut[255]; 
+  } else {
+    return sqrt_lut[index];
+  }
 }
 
 // NOTE pramater a can be bigger 
 // than one up to certain extend
 // this is intended to be used 
 // in the detector function
-int fmul(uint32_t a, int b){
-  // TODO add missing overflow exception
-  uint32_t tmp = (a*b + (1UL << (Q-1)));
-  return (int)(tmp >> Q);
+int fmul(int32_t a, int16_t b){
+
+  int32_t tmp = a*b;
+  Serial.print("fmul a: ");
+  Serial.print(a);
+  Serial.print(", ");
+  Serial.print("fmul b: ");
+  Serial.print(b);
+  // Serial.print(", ");
+  // Serial.print("tmp: ");
+  // Serial.print(tmp);
+
+  tmp += (tmp >= 0) ? (1 << (Q - 1)) : -(1 << (Q - 1));
+
+  Serial.print(", ");
+  Serial.print("fmul rounding: ");
+  Serial.print(tmp);
+  Serial.print(", ");
+  Serial.print("fmul result: ");
+
+  // NOTE: due to the C/C++ signed/unsigned type implementation 
+  // shifting negative number will never reach 0 but always -1, 
+  // therefore we have to check the condition manually, by
+  // checking the abs() of the vaule
+  if(!(abs(tmp) >> Q)) {
+    Serial.println(0);
+    return 0;
+  }
+
+  tmp >>= Q;
+
+  // saturation
+  if (tmp > ONE_Q) {
+    Serial.println(tmp);
+    return (int) ONE_Q;
+  } else if (tmp < -(ONE_Q + 1)) {
+    Serial.println(tmp);
+    return (int) -(ONE_Q + 1);
+  }
+
+  Serial.println(tmp);
+  return (int)tmp;
 }
 
 void detector_init(ZSD_t *d){
@@ -79,59 +124,67 @@ void detector_init(ZSD_t *d){
 }
 
 int detector_update( ZSD_t *d, int value){
-    int delta = value;
     int event = 0;
     Serial.print("value: ");
-    Serial.println(delta);
-
-    // save previous states before updating
-    int last = d->buffer[d->head];
-    int last_avg = d->avg;
+    Serial.println(value);
 
     // classification
-    int input = delta - d->avg;
-
+    int input = abs(value - (d->avg >> ZERO_SCORE_DIV));
     int limit = fmul(ZERO_SCORE_THRES, fsqrt(d->var));
-    int ema_a = fmul(ZERO_SCORE_ALPHA, delta);
-    int ema_b = fmul((ONE_Q - ZERO_SCORE_ALPHA), d->filtered);
 
-    Serial.print("input: ");
-    Serial.print(input);
-    Serial.print(", ");
     Serial.print("limit: ");
     Serial.print(limit);
     Serial.print(", ");
-    Serial.print("ema_a: ");
-    Serial.print(ema_a);
-    Serial.print(", ");
-    Serial.print("ema_b: ");
-    Serial.println(ema_b);
+    Serial.print("input: ");
+    Serial.println(input);
 
-    if ((abs(input) > limit) && (limit > 0)){
-       if (input > ZERO_SCORE_NOISE) event = 1;
-       d->filtered = ema_a + ema_b;
+    if ((input > limit) && (limit > 0)){
+      d->filtered += fmul(ZERO_SCORE_ALPHA, (value - d->filtered));
+
+      if ((value > (d->avg >> ZERO_SCORE_DIV)) && (value > ZERO_SCORE_NOISE))
+        event = 1;
+
     } else {
-       d->filtered = delta;
+       d->filtered = value;
     }
 
+    // save previous states before updating
+    int l_avg = d->avg;
+    int aa = d->filtered - d->buffer[d->head];
+
     // update average and variance
-    d->avg += ((d->filtered - last) >> 2); // NOTE don't forget to change it
-    
-    int a = d->filtered - last;
-    int b = d->filtered - d->avg + last - last_avg;
-    d->var += fmul(a, b) >> 2;
+    d->avg += aa;
+    int c = ((d->avg + l_avg) >> ZERO_SCORE_DIV);
+    int bb = d->filtered - c + d->buffer[d->head];
+    d->var += fmul((aa << ZERO_SCORE_EXTEND), (bb << ZERO_SCORE_EXTEND));
+
+    // variance cannot be negative
+    if (d->var < 0)
+      d->var = 0;
+
+    Serial.print("aa: ");
+    Serial.print(aa);
+    Serial.print(", ");
+    Serial.print("bb: ");
+    Serial.print(bb);
+    Serial.print(", ");
+    Serial.print("extend aa: ");
+    Serial.print(aa << ZERO_SCORE_EXTEND);
+    Serial.print(", ");
+    Serial.print("extend bb: ");
+    Serial.println(bb << ZERO_SCORE_EXTEND);
 
     Serial.print("filtered: ");
     Serial.print(d->filtered);
     Serial.print(", ");
     Serial.print("avg: ");
-    Serial.print(d->avg);
+    Serial.print(d->avg >> ZERO_SCORE_DIV);
     Serial.print(", ");
     Serial.print("var: ");
-    Serial.print(d->var);
-    Serial.print(", ");
+    Serial.println(d->var);
     Serial.print("stddev: ");
     Serial.println(fsqrt(d->var));
+    Serial.println("");
 
     // update circular buffer
     d->buffer[d->head] = d->filtered;
@@ -141,12 +194,16 @@ int detector_update( ZSD_t *d, int value){
 }
 
 // global variables
-int data [DATA_LENGTH] = {400, 580, 600, 800, 1000, 1020, 200, 300, 443, 700, 100};
+int data [DATA_LENGTH] = {-1904, -1909, -1914, -1921, -1929, -1939, 
+                          -1950, -1960, -1968, -1977, -1980};
 int index = 0;
 int tmp = 0;
 ZSD_t detector;
 
 void setup() {
+  // calculations
+  int aa = 27;
+  int bb = -7;
 
   // initialize console
   Serial.begin(115200);
@@ -160,8 +217,8 @@ void setup() {
 
 void loop() {
   if(index < DATA_LENGTH){
-    tmp = ADC_TO_Q115(data[index]);
-    detector_update(&detector, tmp); 
+    //tmp = ADC_TO_Q115(data[index]);
+    detector_update(&detector, data[index]); 
     ++index;
   }
 }
